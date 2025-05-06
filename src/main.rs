b5a42aa3246f61084f9b50281af9e1a60e9f836e
Dominik <dominik.malkowiak@gmail.com>
2025-05-07 00:59:05 +0200

    feat: app mod with basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-  println!("Hello, world!");
+mod app;
+
+
+use std::error::Error;
+
+use winit::event_loop::EventLoop;
+
+
+fn main() -> Result<(), impl Error> {
+  let event_loop = EventLoop::new().unwrap();
+  let mut app = app::App::new(&event_loop);
+
+  event_loop.run_app(&mut app)
 }
+
