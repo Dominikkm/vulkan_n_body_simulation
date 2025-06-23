cb69d94e5068721a1b78f507d46ccc691174fb3b
Dominik <dominik.malkowiak@gmail.com>
2025-06-24 01:40:53 +0200

    examples: add basic examples

---
+++ examples/galaxy/main.rs
@@ -0,0 +1,20 @@
+use vulkan_n_body_simulation::{
+  config,
+  app,
+  parse_args,
+};
+
+use winit::event_loop::EventLoop;
+
+use std::error::Error;
+
+
+fn main() -> Result<(), impl Error> {
+  let config_path = parse_args().unwrap_or("./examples/galaxy/config.toml".into());
+  let config = config::Config::new(&config_path);
+  let event_loop = EventLoop::new().unwrap();
+  let mut app = app::App::new(&event_loop, config);
+
+  event_loop.run_app(&mut app)
+}
+
